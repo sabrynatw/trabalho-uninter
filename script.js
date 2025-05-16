af015e1437b4f2baf4a99534140f014fc82e7aad
Sabryna <160426176+sabrynatw@users.noreply.github.com>
2025-05-16 02:43:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mensagemBtn').addEventListener('click', function() {
+    // Quando clicar, aparece a mensagem abaixo do botão
+    document.getElementById('mensagem').textContent = "Bem-vindo(a) que seu dia seja maravilhoso!";
+});
